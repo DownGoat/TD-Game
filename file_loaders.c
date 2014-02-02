401debba596d5ca10524544f146de47705c530ec
DownGoat <sindre@downgoat.net>
2014-02-02 03:18:27 +0000

    Space and tabs issues

--- file_loaders.c
+++ file_loaders.c
@@ -6,25 +6,25 @@
 
 SDL_Surface *load_image(char *filename) {
 	SDL_Surface* loadedImage = NULL;
-    SDL_Surface* optimizedImage = NULL;
+	SDL_Surface* optimizedImage = NULL;
 
-    loadedImage = IMG_Load(filename);
+	loadedImage = IMG_Load(filename);
 
-    if(loadedImage != NULL) {
-        optimizedImage = SDL_DisplayFormat(loadedImage);
+	if(loadedImage != NULL) {
+		optimizedImage = SDL_DisplayFormat(loadedImage);
 
-        SDL_FreeSurface(loadedImage);
+		SDL_FreeSurface(loadedImage);
 
-        if(optimizedImage != NULL) {
-            SDL_SetColorKey(
-            	optimizedImage,
-            	SDL_SRCCOLORKEY,
-            	SDL_MapRGB(optimizedImage->format, 0, 0xFF, 0xFF)
-            	);
-        }
-    }
+		if(optimizedImage != NULL) {
+			SDL_SetColorKey(
+				optimizedImage,
+				SDL_SRCCOLORKEY,
+				SDL_MapRGB(optimizedImage->format, 0, 0xFF, 0xFF)
+				);
+		}
+	}
 
-    return optimizedImage;
+	return optimizedImage;
 }
 
 
@@ -39,17 +39,17 @@ SDL_Rect *create_tileset(struct tilesheet *sheet) {
 		printf("Width %d, Height %d\n", width, height);
 
 		clip[ 0 ].x = width;;
-	    clip[ 0 ].y = height;
-	    clip[ 0 ].w = sheet->tile_width;
-	    clip[ 0 ].h = sheet->tile_height;
+		clip[ 0 ].y = height;
+		clip[ 0 ].w = sheet->tile_width;
+		clip[ 0 ].h = sheet->tile_height;
 
-	    if (width >= sheet->img_width - sheet->tile_width) {
+		if (width >= sheet->img_width - sheet->tile_width) {
 			width = 0;
 			height += sheet->tile_height;
 		}
 
 		else {
-			width += sheet->tile_width;
+				width += sheet->tile_width;
 		}
 	}
 
